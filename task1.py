7a01bad52224f17e8e5f486020bf85ce59c5ebf6
Harish752 <50616095+Harish752@users.noreply.github.com>
2019-05-15 11:01:39 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,43 @@
+print ("enter the values of a &b")
+
+a=str(input())
+
+b=str(input())
+
+if a.isalpha() or b.isalpha():
+
+	print("is a alphabet")
+
+print("enter the operator choice")
+
+ch=int(input())
+
+if ch == 1:
+   
+	print(a+b)
+
+elif ch == 2:
+   
+	print(a-b)
+
+elif ch == 3:
+  
+	print(a*b)
+
+elif ch == 4:
+   
+	if b == 0:
+     
+		print("infinity")
+  
+	else:
+        
+		print(a/b)    
+
+elif ch == 5:
+    
+	print(a%b)
+
+else:
+    
+	print("invalid")
