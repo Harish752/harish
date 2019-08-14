85169514948ac51f75d66b1fd5eec14d9628f8d2
Harish752 <50616095+Harish752@users.noreply.github.com>
2019-08-14 21:22:20 +0530

    Update pl7.py

--- pl7.py
+++ pl7.py
@@ -1,8 +1,8 @@
-xy=list(map(str,input()))
-t=""
-for i in range(0,len(xy)-1,2):
-    t=xy[i]
-    xy[i]=xy[i+1]
-    xy[i+1]=t
-for i in range(0,len(xy):
-    print(xy[i],end="")
+n=list(map(str,input()))
+p=""
+for i in range(0,len(n)-1,2):
+    t=n[i]
+    n[i]=n[i+1]
+    n[i+1]=p
+for i in range(0,len(n)):
+    print(n[i],end="")
