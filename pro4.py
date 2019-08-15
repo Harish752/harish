25e83eec6d3a50ec85404fb3241d13c0a87a3690
Harish752 <50616095+Harish752@users.noreply.github.com>
2019-08-15 16:27:28 +0530

    Update pro4.py

--- pro4.py
+++ pro4.py
@@ -1,12 +1,12 @@
-n,k=map(str,input().split())
-ab=0
-if len(n)==len(k):
-    for i in range(0,len(n)):
-        ab=ab+(abs(ord(n[i])-ord(k[i])))
+ab,cd=map(str,input().split())
+s=0
+if len(ab)==len(cd):
+    for i in range(0,len(ab)):
+        s=s+(abs(ord(ab[i])-ord(cd[i])))
 else:
-    for i in range(0,len(n)):
-        ab=ab+(abs(ord(n[i])-ord(k[i])))
-    d=abs(len(n)-len(k))
+    for i in range(0,len(ab)):
+        s=s+(abs(ord(ab[i])-ord(cd[i])))
+    d=abs(len(ab)-len(cd))
     for i in range(0,d):
-        ab=ab+ord(k[len(n)+i])-96
-print(ab)
+        s=s+ord(cd[len(ab)+i])-96
+print(s)
